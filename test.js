0debbc6efc5b9f2a8432d6ac5d27f88a4180a975
diellzajusufi <dj30832@seeu.edu.mk>
2026-03-24 15:36:21 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+function calculateTotal(price, tax) {
+    let total = price + tax
+
+    if(total > 100) {
+        console.log("Expensive")
+    }
+
+    if(total > 100) {
+        console.log("Very Expensive")
+    }
+
+    return totl
+}
+
+let result = calculateTotal(50, "20")
+console.log(result)
